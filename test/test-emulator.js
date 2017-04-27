33913c9d65c3df7ba0ac68fcbb52b8782aa95a68
Christopher Williams <chris.a.williams@gmail.com>
2017-04-27 15:53:35 -0400

    Bump up emulator detect timeout

--- test/test-emulator.js
+++ test/test-emulator.js
@@ -34,7 +34,7 @@ describe('emulator', function () {
 	});
 
 	(process.platform === 'win32' ? it : it.skip)('detect Windows Phone emulators', function (done) {
-		this.timeout(5000);
+		this.timeout(8000);
 		this.slow(4000);
 
 		windowslib.emulator.detect(function (err, results) {
