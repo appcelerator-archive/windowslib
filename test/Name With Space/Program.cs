4e9423457ce2303135ebddcdc319a0244ae37677
Ewan Harris <ewanharris93@gmail.com>
2017-11-23 15:10:21 +0000

    [DAEMON-184] Wrap project name in quotes
    Try longer timeout
    
    Remove only
    
    Updategitignore

---
+++ test/Name With Space/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_With_Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
